36dd92d5b463f38b56594ee3897a76618cee0a2b
Niklas Baumstark <niklas.baumstark@gmail.com>
2013-11-18 00:05:17 +0100

    update FFT

--- arithmetik/fft.cpp
+++ arithmetik/fft.cpp
@@ -1,21 +1,22 @@
-typedef complex<double> cplx;
+typedef double D; // or long double?
+typedef complex<D> cplx; // use own implementation for 2x speedup
+const D pi = acos(-1); // or -1.L for long double
+
 // input should have size 2^k
 vector<cplx> fft(const vector<cplx>& a, bool inv=0) {
-    int len=1, n=a.size();
+    int logn=1, n=a.size();
     vector<cplx> A(n);
-    while((1<<len)<n) len++;
-    for(int i=0; i<n; i++) {
-        int j=0;
-        for(int k=0; k<len; k++)
-            if(i & (1<<k))
-                j |= (1<<(len-1-k));
+    while((1<<logn)<n) logn++;
+    rep(i,0,n) {
+        int j=0; // precompute j = rev(i) if FFT is used more than once
+        rep(k,0,logn) j = (j<<1) | ((i>>k)&1);
         A[j] = a[i]; }
     for(int s=2; s<=n; s<<=1) {
-        double ang = 2 * pi / s * (inv ? -1 : 1);
+        D ang = 2 * pi / s * (inv ? -1 : 1);
         cplx ws(cos(ang), sin(ang));
         for(int j=0; j<n; j+=s) {
             cplx w=1;
-            for(int k=0; k<s/2; k++) {
+            rep(k,0,s/2) {
                 cplx u = A[j+k], t = A[j+s/2+k];
                 A[j+k] = u + w*t;
                 A[j+s/2+k] = u - w*t;
@@ -23,7 +24,7 @@ vector<cplx> fft(const vector<cplx>& a, bool inv=0) {
                 w *= ws; } } }
     return A;
 }
-vector<cplx> a = {1,2,3,4}, b = {2,3,0,1}; // polynomials
+vector<cplx> a = {0,0,0,0,1,2,3,4}, b = {0,0,0,0,2,3,0,1}; // polynomials
 a = fft(a); b = fft(b);
-for (int i = a.size()-1; i >= 0; --i) a[i] *= b[i]; // convult spectrum
+rep(i,0,a.size()) a[i] *= b[i]; // convult spectrum
 a = fft(a,1); // ifft, a = a * b
