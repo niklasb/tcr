9294293aa41c4eb069a5d20c7873fbc2720673ae
Niklas Baumstark <niklas.baumstark@gmail.com>
2013-11-18 00:09:33 +0100

    add C++ template and update title

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+using ll=long long;
+using ld=long double;
+using pii=pair<int,int>;
+using vi=vector<int>;
+#define rep(i,s,e) for (int i=(s);i<(e);++i)
+#define pb push_back
+#define mk make_pair
+#define fst first
+#define snd second
+#define all(x) (x).begin(),(x).end()
+#define clr(x,y) memset(x,y,sizeof x)
+#define contains(x,y) (x).find(y)!=(x).end()
+#define uset unordered_set
+#define umap unordered_map
+const int dx[]={0,0,1,-1,1,-1,1,-1}, dy[]={-1,1,0,0,1,-1,-1,1};
