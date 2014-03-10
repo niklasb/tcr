e2805730761e608e74ea46b45b6b8ef0902e00ab
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-03-09 23:34:48 -0400

    fix stuff, add theorem about degree sequence of simple graphs

--- datenstrukturen/fenwick.cpp
+++ datenstrukturen/fenwick.cpp
@@ -1,13 +1,13 @@
 const int n = 100;
-static int tree[n+1];
+
 // get v[idx] (1 <= idx <= n)
-int read(int idx){
+int read_idx(int tree[], int idx) {
   int sum = 0;
-  for (; idx > 0; idx -= idx&-idx) sum += tree[idx];
+  for (; idx <= n; idx += idx & -idx) sum += tree[idx];
   return sum;
 }
 // update v[1..idx] += val
-void update(int idx, int val){
-  int sum = 0;
-  for (; idx <= n; idx += idx&-idx) tree[idx] += val;
+void update_prefix(int tree[], int idx, int val) {
+  if (idx < 1) return;
+  for (; idx > 0; idx -= idx & -idx) tree[idx] += val;
 }
