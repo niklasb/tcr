a35c655f6b8d9b886d3609dc819b2ddab95b4078
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-05-21 18:01:56 +0200

    fix fenwick tree

--- datenstrukturen/fenwick.cpp
+++ datenstrukturen/fenwick.cpp
@@ -1,13 +1,65 @@
-const int n = 100;
+const int n = 10;
 
-// get v[idx] (1 <= idx <= n)
-int read_idx(int tree[], int idx) {
+// mode 1: update indices, read prefixes
+void update_idx(int tree[], int i, int val) { // v[i] += val
+  for (; i <= n; i += i & -i) tree[i] += val;
+}
+int read_prefix(int tree[], int i) { // get sum v[1..i]
   int sum = 0;
-  for (; idx <= n; idx += idx & -idx) sum += tree[idx];
+  for (; i > 0; i -= i & -i) sum += tree[i];
   return sum;
 }
-// update v[1..idx] += val
-void update_prefix(int tree[], int idx, int val) {
-  if (idx < 1) return;
-  for (; idx > 0; idx -= idx & -idx) tree[idx] += val;
+int kth(int k) { // find kth element in tree (1-based index)
+  int ans = 0;
+  for (int i = maxl; i >= 0; --i) // maxl = largest i s.t. (1<<i) <= n
+    if (ans + (1<<i) <= N && tree[ans + (1<<i)] < k) {
+      ans += 1<<i;
+      k -= tree[ans];
+    }
+  return ans+1;
+}
+
+// mode 2: update prefixes, read indices
+void update_prefix(int tree[], int i, int val) { // v[1..i] += val
+  for (; i > 0; i -= i & -i) tree[i] += val;
+}
+int read_idx(int tree[], int i) { // get v[i]
+  int sum = 0;
+  for (; i <= n; i += i & -i) sum += tree[i];
+  return sum;
+}
+
+// mode 3: range-update range-query (using point-wise of linear functions)
+const int maxn = 100100;
+int n;
+ll mul[maxn], add[maxn];
+
+void update_idx(ll tree[], int x, ll val) {
+  for (int i = x; i <= n; i += i & -i) tree[i] += val;
+}
+void update_prefix(int x, ll val) { // v[x] += val
+  update_idx(mul, 1, val);
+  update_idx(mul, x + 1, -val);
+  update_idx(add, x + 1, x * val);
+}
+ll read_prefix(int x) { // get sum v[1..x]
+  ll a = 0, b = 0;
+  for (int i = x; i > 0; i -= i & -i) a += mul[i], b += add[i];
+  return a * x + b;
+}
+void update_range(int l, int r, ll val) { // v[l..r] += val
+  update_prefix(l - 1, -val);
+  update_prefix(r, val);
+}
+ll read_range(int l, int r) { // get sum v[l..r]
+  return read_prefix(r) - read_prefix(l - 1);
+}
+
+#include <iostream>
+using namespace std;
+int tree[n+1];
+int main() {
+  update_prefix(tree, 3, 1);
+  update_prefix(tree, 5, 3);
+  for (int i = 1; i <= n; ++ i) cout << read_prefix(tree, i) << " ";cout<<endl;
 }
