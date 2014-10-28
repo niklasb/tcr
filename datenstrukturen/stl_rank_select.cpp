5c13b5b3ea84ab282bb114af9d42b5cfbb21ab91
Niklas Baumstark <niklas.baumstark@gmail.com>
2014-10-29 00:05:24 +0100

    add STL rank/select

---
+++ datenstrukturen/stl_rank_select.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std; using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> Tree;
+int main() {
+    Tree X;
+    for (int i = 1; i <= 16; i <<= 1) X.insert(i); // { 1, 2, 4, 8, 16 };
+    cout << *X.find_by_order(3) << endl; // => 8
+    cout << X.order_of_key(3) << endl; // => 4 = successor of 10 = min i such that X[i] >= 10
+}
