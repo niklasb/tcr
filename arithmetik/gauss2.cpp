082761ad1e0ff6e2a8fa8fca0933b316fc72a7e7
Niklas Baumstark <niklas.baumstark@gmail.com>
2015-11-23 18:10:59 +0100

    Update and rename gauss2.java to gauss2.cpp

--- arithmetik/gauss2.cpp
+++ arithmetik/gauss2.cpp
@@ -1,28 +1,38 @@
-class R {
-	BigInteger n, d;
-	R(BigInteger n_, BigInteger d_) {
+struct R {
+	ll n, d; // or use BigInteger in Java
+	R(ll n_=0, ll d_=1) {
 		n = n_; d = d_;
-		BigInteger g = n.gcd(d);
-		n.divide(g); d.divide(g);
+		ll g = __gcd(n,d);
+		n/=g;
+		d /= g;
+		if (d < 0){
+			n=-n;
+			d=-d;
+		}
 	}
 	R add(R x) {
-		return new R(n.multiply(x.d).add(d.multiply(x.n)), d.multiply(x.d));
+		return R(n * x.d + d*x.n, d * x.d);
 	}
-	R negate() { return new R(n.negate(), d); }
+	R negate() { return R(-n, d); }
 	R subtract(R x) { return add(x.negate()); }
-	R multiply(R y) {
-		return new R(n.multiply(x.n), d.multiply(x.d));
+	R multiply(R x) {
+		return R(n * x.n, d * x.d);
 	}
-	R invert() { return new R(d, n); }
+	R invert() { return R(d, n); }
 	R divide(R y) { return multiply(y.invert()); }
-	boolean zero() { return d.equals(BigInteger.ZERO); }
+	bool zero() { return !n; }
+};
+
+void normalize_row(int i, int cols) {
+	ll g = 0;
+	for (int j = 0; j < cols; ++j)
+		g = __gcd(g, M[i][j].n);
+	if (g == 0)return;
+	for (int j = 0; j < cols; ++j)
+		M[i][j].n /= g;
 }
 
-int maxm = 13, maxn = 4;
-R[][] M = new R[maxm][maxn]; // the LGS matrix
-R[] B = new R[maxm];         // the right side
-
-void gauss(int m, int n) { // reduces M to Gaussian normal form
+void gauss(int m, int n) { // m=rows, n=cols, reduces M to Gaussian normal form
 	int row = 0;
 	for (int col = 0; col < n; ++col) { // eliminate downwards
 		int pivot=row;
@@ -38,7 +48,7 @@ void gauss(int m, int n) { // reduces M to Gaussian normal form
 			B[row] = B[pivot];
 			B[pivot] = tmp;
 		}
-		// for double, normalize pivot row here (divide it by pivot value)
+		normalize_row(row, n); // to avoid overflows. also use in case of double
 		for (int j = row+1; j < m; ++j) {
 			if (M[j][col].zero()) continue;
 			R a = M[row][col], b = M[j][col];
@@ -48,13 +58,10 @@ void gauss(int m, int n) { // reduces M to Gaussian normal form
 		}
 		row++;
 	}
-	for (int col = 0; col < n; ++col) { // eliminate upwards
-		for (row = m-1; row >= 0; --row) {
+	for (int row = m-1; row >= 0; --row) { // eliminate upwards
+		normalize_row(row, n);
+		for (int col = 0; col < n; ++col) {
 			if (M[row][col].zero()) continue;
-			boolean valid=true;
-			for (int j = 0; j < col; ++j)
-				if (!M[row][j].zero()) { valid=false; break; }
-			if (!valid) continue;
 			for (int i = 0; i < row; ++i) {
 				R a = M[row][col], b = M[i][col];
 				for (int k =0; k<n; ++k)
@@ -64,4 +71,16 @@ void gauss(int m, int n) { // reduces M to Gaussian normal form
 			break;
 		}
 	}
+}
+
+int getrank() {
+	int rank = 0;
+	for (int i = 0; i < m; ++i) {
+		bool valid = 0;
+		for (int j=0;j<n;++j)
+			if (!M[i][j].zero())
+				valid=1;
+		rank += valid?1:0;
+	}
+	return rank;
 }
