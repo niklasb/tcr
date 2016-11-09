c08133d73c6ccc6bf7c810e88dc25a5887145f0a
Niklas Baumstark <niklas.baumstark@gmail.com>
2016-11-09 20:57:52 +0100

    bipartite matching

--- graphentheorie/BipartiteMatching2.cpp
+++ graphentheorie/BipartiteMatching2.cpp
@@ -1,23 +1,22 @@
 // run time: O(n * min(ans^2, |E|)), where n is the size of the left side
-vector<int> madj[1001]; // adjacency list
-int pairs[1001]; // for every node, stores the matching node on the other side or -1
-bool vis[1001];
-bool dfs(int i) {
-    if (vis[i]) return 0;
-    vis[i] = 1;
-    foreach(it,madj[i]) {
-        if (pairs[*it] < 0 || dfs(pairs[*it])) {
-            pairs[*it] = i, pairs[i] = *it;
+vector<int> adj[1001]; // adjacency list
+int iter, match[1001], vis[1001];
+bool dfs(int x) {
+    if (vis[x] == iter) return 0;
+    vis[x] = iter;
+    for (auto y : adj[x]) {
+        if (match[y] < 0 || dfs(match[y])) {
+            match[y] = x, match[x] = y;
             return 1;
         }
     }
     return 0;
 }
 int kuhn(int n) { // n = nodes on left side (numbered 0..n-1)
-    clr(pairs,-1); // to accelerate, just initialize with a greedy matching
+    memset(match,-1,sizeof match) ;// to accelerate, initialize with a greedy matching
     int ans = 0;
-    rep(i,0,n) {
-        clr(vis,0);
+    for (int i = 0; i < n; ++i) {
+        ++iter;
         ans += dfs(i);
     }
     return ans;
